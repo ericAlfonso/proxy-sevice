64e987b5fa79b5670611e11b9c54fb537e7c9133
eric alfonso <developer.eric@blackdish.mx>
2021-08-21 20:28:55 -0500

    application bootstrap

---
+++ src/test/java/com/proxy/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxy.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
